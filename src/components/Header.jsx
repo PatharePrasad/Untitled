222c8373a8018a7efa417d5fbe2b94cf6a4971b6
Prasad Pathare <prasadsachinist10@gmail.com>
2024-12-09 14:26:20 +0000

    layout done - added favicon, custom font

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex items-center gap-2">
+      <img src="favicon.png" className="w-10" />
+      <p className="text-lg font-semibold">Untitled UI</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Aside from "../Aside";
+import Form from "../Form";
+import Header from "../Header";
+
+function Home() {
+    return (
+      <div>
+        <Header />
+        <Aside />
+        <Form />
+      </div>
+    );
+  }
+  
+  export default Home;
