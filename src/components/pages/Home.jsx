3e65a2f35c21e09bd1a197e9b12c896b852bcdc1
Prasad Pathare <prasadsachinist10@gmail.com>
2024-12-10 14:37:40 +0000

    Form UI Complete

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,17 +3,20 @@ import Form from "@/components/Form";
 import Header from "@/components/Header";
 
 function Home() {
-    return (
-      <div className="min-h-screen bg-zinc-50 px-4 py-3 md:px-8">
-        <Header />
-        <main className="grid grid-cols-8 bg-red-100">
-          <div className="col-span-2 hidden bg-green-300 md:block">
-        <Aside />
+  return (
+    <div className="min-h-screen bg-zinc-50 px-4 py-3 md:px-8">
+      <Header />
+      <main className="grid grid-cols-8">
+        <div className="col-span-2 hidden md:block">
+          <Aside />
         </div>
-        <Form />
-        </main>
-      </div>
-    );
-  }
+        <div className="col-span-full rounded-lg p-5 md:col-span-6 md:bg-lime-400">
+          <Form />
+        </div>
+      </main>
+    </div>
+  );
+}
+
   
   export default Home;
