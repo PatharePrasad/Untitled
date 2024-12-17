40478141334eb2691c86615ea8a2b7aa9b8674b0
Prasad Pathare <prasadsachinist10@gmail.com>
2024-12-17 14:16:10 +0000

    Using react-hook-form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,75 +1,90 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { RiSparklingFill } from "react-icons/ri";
 import Intro from "@/components/Intro";
 import config from "../utils/config";
+import { data } from "autoprefixer";
+
+const services = [
+  "Website Design",
+  "Content",
+  "UX Design",
+  "Strategy",
+  "User Research",
+  "Other",
+];
 
 function Form() {
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [selectedServices, setSelectedServices] = useState([]);
-
-  const services = [
-    "Website Design",
-    "Content",
-    "UX Design",
-    "Strategy",
-    "User Research",
-    "Other",
-  ];
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(selectedServices);
-  };
-
-  // @desc This function is invoked by clicking on checkbox
-  // @desc Logs the value
-  const handleCheckbox = (value, checked) => {
-    //console.log(value, checked)
-    setSelectedServices((prevState) => {
-      const updatedServices = checked
-        ? [...prevState, value]
-        : prevState.filter((service) => service !== value);
-      return updatedServices;
-    });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      fullname: "",
+      email: "",
+      message: "",
+      services: [],
+    },
+  });
 
   return (
     <>
       <Intro />
       <form
         className="flex flex-col gap-1"
-        action={config.submitURL}
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
       >
         {/* Inputs */}
         <input
           type="text"
-          name={config.fullname}
+          {...register("fullname", {
+            required: "Please enter your full name",
+            minLength: {
+              value: 4,
+              message: "Kaafi chota naam hai tumhara",
+            },
+          })}
           id="fullname"
           placeholder="Your name"
           className="border-b border-stone-700 p-2 placeholder-gray-700 md:bg-lime-400"
-          value={fullname}
-          onChange={(e) => setFullname(e.target.value)}
         />
+        {errors.fullname && (
+          <p className="text-red-500">{errors.fullname.message}</p>
+        )}
+
         <input
           type="email"
-          name={config.email}
+          {...register("email", {
+            required: "Please enter your email!",
+            pattern: {
+              value: /[\w]*@*[a-z]*\.*[\w]{5,}(\.)*(com)*(@gmail\.com)/,
+              message: "Temp mail ke chakkar mein mat reh, gmail daal de",
+            },
+          })}
           id="email"
           placeholder="you@company.com"
           className="border-b border-stone-700 p-2 placeholder-gray-700 md:bg-lime-400"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+
         <input
           type="text"
-          name={config.message}
+          {...register("message", {
+            required: "Enter a message dear!",
+            minLength: {
+              value: 5,
+              message: "Jada chota nhi hogya?",
+            },
+          })}
           id="message"
           placeholder="Tell us a bit about your project..."
           className="h-24 border-b border-stone-700 p-2 placeholder-gray-700 md:bg-lime-400"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
         />
+        {errors.message && (
+          <p className="text-red-500">{errors.message.message}</p>
+        )}
 
         <p className="my-5 text-gray-800">How can we help?</p>
 
@@ -80,15 +95,19 @@ function Form() {
               <label key={idx} className="flex cursor-pointer gap-2">
                 <input
                   type="checkbox"
-                  name={config.services}
                   value={service}
+                  {...register("services", {
+                    required: "Enter atleast one!",
+                  })}
                   className="size-5"
-                  onClick={(e) => handleCheckbox(service, e.target.checked)}
                 />
                 {service}
               </label>
             );
           })}
+          {errors.services && (
+            <p className="text-red-500">{errors.services.message}</p>
+          )}
         </div>
 
         {/* Submit */}
@@ -96,7 +115,7 @@ function Form() {
           type="submit"
           className="flex items-center justify-center gap-2 rounded bg-zinc-950 p-2 text-white"
         >
-          Let's get started!{" "}
+          Let's get started
           <RiSparklingFill className="text-lime-500" size={20} />
         </button>
       </form>
